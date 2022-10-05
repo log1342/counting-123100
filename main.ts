f5468b31347e27bd9af6a298758b882ad0488e67
log1342 <113910996+log1342@users.noreply.github.com>
2022-10-05 08:57:04 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 100; index++) {
+        counter += 1
+        sum = sum + counter
+    }
+    basic.showNumber(sum)
+})
+let sum = 0
+let counter = 0
+counter = 0
+basic.showNumber(counter)
+sum = 0
+basic.forever(function () {
+	
+})
